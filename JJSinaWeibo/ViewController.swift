b4afe6cc2a90c456d5be1f685ff88d7b959bc2c3
金占军 <519511646@qq.com>
2019-07-06 11:20:05 +0800

    Initial Commit

---
+++ JJSinaWeibo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JJSinaWeibo
+//
+//  Created by 金占军 on 2019/7/6.
+//  Copyright © 2019 金占军. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
